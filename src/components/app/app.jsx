2967277a57c74ad96f35dc3267fe10f9e26d6447
Sergey <perm.it.limon@gmail.com>
2020-10-07 23:28:42 +0500

    Навигация

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,39 @@
 import React from "react";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import PropTypes from "prop-types";
 import WelcomeScreen from "../welcome-screen/welcome-screen";
+import AuthScreen from "../auth-screen/auth-screen";
+import ArtistQuestionScreen from "../artist-question-screen/artist-question-screen";
+import GenreQuestionScreen from "../genre-question-screen/genre-question-screen";
+import WinScreen from "../win-screen/win-screen";
+import GameOverScreen from "../game-over-screen/game-over-screen";
 
 const App = (props) => {
   const {errorsCount} = props;
 
   return (
-    <WelcomeScreen errorsCount={errorsCount} />
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <WelcomeScreen errorsCount={errorsCount} />
+        </Route>
+        <Route path="/login" exact>
+          <AuthScreen />
+        </Route>
+        <Route path="/dev-artist" exact>
+          <ArtistQuestionScreen />
+        </Route>
+        <Route path="/dev-genre" exact>
+          <GenreQuestionScreen />
+        </Route>
+        <Route path="/result" exact>
+          <WinScreen />
+        </Route>
+        <Route path="/lose" exact>
+          <GameOverScreen />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
